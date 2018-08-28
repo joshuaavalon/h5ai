35da50583f0ce5d3ea8e99831452e82b1f9adc2d
Joshua Avalon <joshuaavalon@gmail.com>
2018-08-28 16:31:05 +0800

    Fix startup script

--- root/start.sh
+++ root/start.sh
@@ -2,5 +2,5 @@
 
 usermod -u ${PUID:-1000} www-data
 groupmod -g ${PGID:-1000} www-data
-chmod -R www-data:www-data /var/www/html/_h5ai
+chown -R www-data:www-data /var/www/html/_h5ai
 apache2-foreground
